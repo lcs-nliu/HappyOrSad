c79259fb09a5ab4fabd866091a6e511f92856b18
Nicole Liu <nicoleliu_@outlook.com>
2019-11-20 11:32:02 -0500

    Hooked up the view to the controller (outlets and actions created)

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// Mark: Properties
+// Connected text field to controller using an outlet
+    @IBOutlet weak var messageTextField: UITextField!
+// Connected label to controller using an outlet
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// Mark: Methods
+// Created an action for when the button is pressed
+    @IBAction func analyzeButtonPressed(_ sender: Any) {
+    
+    }
 }
 
